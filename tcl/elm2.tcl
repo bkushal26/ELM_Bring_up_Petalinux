269cf03343408e38992de8185f45a791e319296a
bkushal26 <bhalerao.kushal2426@gmail.com>
2021-07-08 05:33:38 -0400

    modified tcl with dual support version

--- tcl/elm2.tcl
+++ tcl/elm2.tcl
@@ -24,11 +24,13 @@ set script_folder [_tcl::get_script_folder]
 # Check if script is running in correct Vivado version.
 ################################################################
 set scripts_vivado_version 2019.1
+set scripts_vivado_version1 2020.1
+
 set current_vivado_version [version -short]
 
-if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+if {( [string first $scripts_vivado_version $current_vivado_version] || [string first $scripts_vivado_version1 $current_vivado_version]) == -1 } {
    puts ""
-   catch {common::send_msg_id "BD_TCL-109" "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
+   catch {common::send_msg_id "BD_TCL-109" "ERROR" "This script was generated using Vivado <$scripts_vivado_version> or <$scipts_vivado_version1> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> or <$scripts_vivado_version1> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
 
    return 1
 }
@@ -1553,32 +1555,55 @@ create_bd_design $design_name
  
 ] $zynq_ultra_ps_e_0
 
-##########################validate the design###################################
+##########################validate the design####################################################################################
   
 
   validate_bd_design
   save_bd_design
   
-#################################################################################
+#################################################################################################################################
 
-##########################Make the Wrapper#####################################################
+##########################Make the Wrapper#######################################################################################
 update_compile_order -fileset sources_1
 make_wrapper -files [get_files myproj/project_1.srcs/sources_1/bd/ps_block/ps_block.bd] -top
 add_files -norecurse myproj/project_1.srcs/sources_1/bd/ps_block/hdl/ps_block_wrapper.v
-###############################################################################################
+##############################################################################################################################
 
-###############################generate output products#######################################################
+###############################generate output products########################################################################
 set_property synth_checkpoint_mode None [get_files myproj/project_1.srcs/sources_1/bd/ps_block/ps_block.bd]
 generate_target all [get_files myproj/project_1.srcs/sources_1/bd/ps_block/ps_block.bd]
 export_ip_user_files -of_objects [get_files myproj/project_1.srcs/sources_1/bd/ps_block/ps_block.bd] -no_script -sync -force -quiet
 export_simulation -of_objects [get_files myproj/project_1.srcs/sources_1/bd/ps_block/ps_block.bd] -directory myproj/project_1.ip_user_files/sim_scripts -ip_user_files_dir myproj/project_1.ip_user_files -ipstatic_source_dir myproj/project_1.ip_user_files/ipstatic -lib_map_path [list {modelsim=myproj/project_1.cache/compile_simlib/modelsim} {questa=myproj/project_1.cache/compile_simlib/questa} {riviera=myproj/project_1.cache/compile_simlib/riviera} {activehdl=myproj/project_1.cache/compile_simlib/activehdl}] -use_ip_compiled_libs -force -quiet
-########################################################################################################################
+#################################################################################################################################
+# add logic for bit + export HW according to version
+# Bit stream
+launch_run synth_1
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream -jobs 2
+wait_on_run impl_1
+puts "bit-stream done,start exporting"
+####################################################################################################################################
+######################################EXPORT HW#####################################################################################
+#old
+#if {$current_vivado_version == $scripts_vivado_version } {
+#file mkdir myproj/project_1.sdk
+#write_hwdef -force  -file myproj/project_1.sdk/ps_block_wrapper.hdf
+##launch_sdk -workspace myproj/project_1.sdk -hwspec myproj/project_1.sdk/ps_block_wrapper.hdf
+#} elseif {$current_vivado_version == $scripts_vivado_version1 } {
+#set_property pfm_name {} [get_files -all {myproj/project_1.srcs/sources_1/bd/ps_block/ps_block.bd}]
+#write_hw_platform -fixed -include_bit -force -file myproj/ps_block_wrapper.xsa
+#}	
 
-#################################Export HW + launch_sdk###########################################################################
-#Note:does not include bit stream
+#new
+if {[string compare $current_vivado_version $scripts_vivado_version] == 0} {
+	file mkdir myproj/project_1.sdk
+	file copy -force myproj/project_1.runs/impl_1/ps_block_wrapper.sysdef myproj/project_1.sdk/ps_block_wrapper.hdf
 
-file mkdir myproj/project_1.sdk
-write_hwdef -force  -file myproj/project_1.sdk/ps_block_wrapper.hdf
+#	write_hwdef -force  -file myproj/project_1.sdk/ps_block_wrapper.hdf
 #launch_sdk -workspace myproj/project_1.sdk -hwspec myproj/project_1.sdk/ps_block_wrapper.hdf
+} elseif {[string compare $current_vivado_version $scripts_vivado_version1] == 0} {
+	set_property pfm_name {} [get_files -all {myproj/project_1.srcs/sources_1/bd/ps_block/ps_block.bd}]
+	write_hw_platform -fixed -include_bit -force -file myproj/ps_block_wrapper.xsa
+}
 
 #########################################################################################################################################
